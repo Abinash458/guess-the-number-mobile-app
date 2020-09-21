36bad5944d784278d1dda24de1b9c7cfbcfee003
Abinash Mohapatra <abinash@expensebit.com>
2020-09-21 22:22:54 +0530

    first commit

---
+++ Screens/StartGameScreen.js
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+
+import Card from "../components/Card";
+import Colors from "../constants/color";
+import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
+
+const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelctedNumber] = useState();
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    }
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    }
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid Number!', 'Number has to be a number between 1 to 99.', [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }])
+            return;
+        }
+        setConfirmed(true);
+        setSelctedNumber(chosenNumber);
+        setEnteredValue('');
+        Keyboard.dismiss();
+    }
+
+    let confirmedOutput;
+
+    if (confirmed) {
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text style={styles.secondCard}>You Selected</Text>
+                <NumberContainer>
+                    {selectedNumber}
+                </NumberContainer>
+                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
+            </Card>
+        )
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text style={styles.cardTitle}>Select a Number</Text>
+                    <Input
+                        style={styles.input}
+                        blurOnSubmit
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        keyboardType="number-pad"
+                        maxLength={2}
+                        onChangeText={numberInputHandler}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title="Reset" onPress={resetInputHandler} color={Colors.secondary} />
+                        </View>
+                        <View style={styles.button}>
+                            <Button title="Confirm" onPress={confirmInputHandler} color={Colors.primary} />
+                        </View>
+                    </View>
+                </Card>
+                {confirmedOutput}
+            </View>
+        </TouchableWithoutFeedback>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+        fontFamily: 'open-sans-bold',
+    },
+    cardTitle: {
+        fontFamily: 'open-sans',
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        paddingHorizontal: 15,
+    },
+    button: {
+        width: 100,
+        marginTop: 10,
+    },
+    input: {
+        width: 50,
+        textAlign: 'center',
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center',
+    },
+    secondCard: {
+        fontFamily: 'open-sans',
+    }
+});
+
+export default StartGameScreen;
